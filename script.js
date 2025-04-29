d61a05f094c2b92eb15127583752653dd8389ccf
Vladyslav <65062517+VladyslavVasylenko@users.noreply.github.com>
2025-04-29 21:19:00 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,14 +1,15 @@
 document.addEventListener("DOMContentLoaded", () => {
-  const buttons = document.querySelectorAll(".lang-button");
-  const elements = document.querySelectorAll("[data-lang]");
+  const buttons = document.querySelectorAll(".lang-button");
+  const contentElements = document.querySelectorAll("main [data-lang]");
 
-  buttons.forEach((button) => {
-    button.addEventListener("click", () => {
-      const selectedLang = button.dataset.lang;
+  buttons.forEach((button) => {
+    button.addEventListener("click", () => {
+      const selectedLang = button.dataset.lang;
 
-      elements.forEach((el) => {
-        el.hidden = el.dataset.lang !== selectedLang;
-      });
-    });
-  });
+      contentElements.forEach((el) => {
+        el.hidden = el.dataset.lang !== selectedLang;
+      });
+    });
+  });
 });
+
